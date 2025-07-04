029a604be1045db5b77041727181a97a28f4475d
Larissarff <larissa.rfferreira4800@gmail.com>
2025-07-04 11:06:01 -0300

    Add deepface_analize.py for facial analysis using DeepFace

---
+++ deepface_analize.py
@@ -0,0 +1,18 @@
+from deepface import DeepFace
+import matplotlib.pyplot as plt
+
+path = r"./DataFrame/tests/teste_03.jpg"
+
+obj = DeepFace.analyze(
+    img_path=path,
+    actions=["age", "gender", "race", "emotion"],
+    enforce_detection=False,
+)
+
+print(obj)
+
+for i in obj:
+    print(f"Age: {i['age']}")
+    print(f"Gender: {i['gender']}")
+    print(f"Race: {i['race']}")
+    print(f"Emotion: {i['emotion']}")
